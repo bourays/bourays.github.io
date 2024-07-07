36fa8b953e8ab0e9da211d0820961171eae69973
bourays <bou.ray@outlook.com>
2024-07-07 20:04:39 +0800

    Site updated: 2024-07-07 20:04:39

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/07/07/hello-world/","2024/07/07/startup/"];function toRandomPost(){
+var posts=["2024/07/07/第一篇博文/","2024/07/07/startup/","2024/07/07/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
